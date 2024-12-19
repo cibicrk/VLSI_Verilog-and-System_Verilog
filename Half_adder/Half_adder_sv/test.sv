8611206641907686eda646395a3662d660506e6b
Cibi Chakkaravarthe R K <114308110+cibicrk@users.noreply.github.com>
2024-12-19 12:23:58 +0530

    test.sv

--- Half_adder/Half_adder_sv/test.sv
+++ Half_adder/Half_adder_sv/test.sv
@@ -1,1 +1,16 @@
+`include "environment.sv"
+class test;
+  environment env;
+  virtual inf vinf;
 
+  // Constructor
+  function new(virtual inf vinf);
+    this.vinf = vinf;
+    env = new(vinf);
+  endfunction
+
+  // Start the environment
+  task start();
+    env.main();
+  endtask
+endclass
